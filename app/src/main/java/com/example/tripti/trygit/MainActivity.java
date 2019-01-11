f192807d77feecd1a9532b38be85d24138a819d6
tripti <tripti97.sharma@gmail.com>
2019-01-11 16:45:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/tripti/trygit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tripti.trygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"just trying git",Toast.LENGTH_LONG).show();
+    }
+
+
+}
